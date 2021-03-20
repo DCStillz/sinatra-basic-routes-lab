75a2004f5018b5d0b92ed450d56dab75ff04629a
DCStillz <dylan@dcokerstills.com>
2021-03-20 12:59:46 -0400

    commit tree dccf707b1c6b99112da761884e38bdeb924d0e05

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Dylan"
+    end
+
+    get '/hometown' do
+        "My hometown is Raleigh"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Change by Blind Melon"
+    end
+    
 end
